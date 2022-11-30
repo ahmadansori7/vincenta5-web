c4cac645730aa9a7d9008ad62bc9b34bb0a76af6
ahmadansori1 <e41210577@student.polije.ac.id>
2022-11-30 08:20:25 +0700

    Update edit-produk.php
    memperbaiki bug upload gambar saat update data.

--- admin/edit-produk.php
+++ admin/edit-produk.php
@@ -13,18 +13,31 @@ if(isset($_POST['update'])) {
     $source = $_FILES ['gambar']['tmp_name'];
     $folder = 'gambarproduk/';
 
-    move_uploaded_file($source, $folder.$nama_file);
-    $query  = mysqli_query($koneksi, "UPDATE `produk` SET `nama_produk`='$nama_produk',`deskripsi_produk`='$deskripsi_produk',`harga`='$harga',`stok`='$stok',`gambar`='$nama_file' WHERE `id_produk`='$id'");
+   
+
+     //Cek Photo
+     if (strlen($nama_file)>0) {
+        //upload Photo
+        if (is_uploaded_file($_FILES['gambar']['tmp_name'])) {
+            move_uploaded_file($source, $folder.$nama_file);
+            $query  = mysqli_query($koneksi, "UPDATE produk SET gambar='$nama_file' WHERE `id_produk`='$id'");
+            $result = mysqli_query($koneksi, $query);
+        }
+    }
+
+
+    $query  = mysqli_query($koneksi, "UPDATE `produk` SET `nama_produk`='$nama_produk',`deskripsi_produk`='$deskripsi_produk',`harga`='$harga',`stok`='$stok'  WHERE `id_produk`='$id'");
     $result = mysqli_query($koneksi, $query);
     echo "<script>
     eval(\"parent.location='produk.php '\");
     alert (' Data Berhasil Diupdate!');
     </script>";
-    
-       
-    }  else {
+
+    } else {
         echo "<script>
         eval(\"parent.location='produk.php '\");
         alert (' Data Gagal Diupdate!');
         </script>";
     }
+
+?>
