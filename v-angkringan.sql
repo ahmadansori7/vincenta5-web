37aa1adfe2db6cf9fc52cf1f9eb414ccd86e64ae
ahmadansori1 <e41210577@student.polije.ac.id>
2022-12-07 13:01:43 +0700

    Create v-angkringan.sql

---
+++ v-angkringan.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2022 at 07:00 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `v-angkringan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dtl_transaksi`
+--
+-- Error reading structure for table v-angkringan.dtl_transaksi: #1932 - Table 'v-angkringan.dtl_transaksi' doesn't exist in engine
+-- Error reading data for table v-angkringan.dtl_transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `v-angkringan`.`dtl_transaksi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+-- Error reading structure for table v-angkringan.produk: #1932 - Table 'v-angkringan.produk' doesn't exist in engine
+-- Error reading data for table v-angkringan.produk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `v-angkringan`.`produk`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+-- Error reading structure for table v-angkringan.transaksi: #1932 - Table 'v-angkringan.transaksi' doesn't exist in engine
+-- Error reading data for table v-angkringan.transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `v-angkringan`.`transaksi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table v-angkringan.user: #1932 - Table 'v-angkringan.user' doesn't exist in engine
+-- Error reading data for table v-angkringan.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `v-angkringan`.`user`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
