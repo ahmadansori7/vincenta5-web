9be9296a505b65cf16b597bc8300b43f118e34ee
ahmadansori1 <e41210577@student.polije.ac.id>
2022-12-08 10:41:27 +0700

    Update angkringan.sql

--- angkringan.sql
+++ angkringan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2022 at 03:21 PM
+-- Generation Time: Dec 08, 2022 at 04:41 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `angkringan`
+-- Database: `angkringanfinal`
 --
 
 -- --------------------------------------------------------
@@ -30,21 +30,17 @@ SET time_zone = "+00:00";
 CREATE TABLE `detail_transaksi` (
   `id_transaksi` char(10) NOT NULL,
   `id_produk` char(10) NOT NULL,
-  `username` varchar(30) NOT NULL,
   `jumlah` int(10) NOT NULL,
-  `totalhargaitem` bigint(20) DEFAULT NULL,
-  `subtotal` bigint(15) DEFAULT NULL,
-  `pengiriman` varchar(150) NOT NULL,
-  `metode` enum('Tunai','COD','Transfer') DEFAULT NULL,
-  `status` enum('0','1') NOT NULL
+  `totalhargaitem` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `detail_transaksi`
 --
 
-INSERT INTO `detail_transaksi` (`id_transaksi`, `id_produk`, `username`, `jumlah`, `totalhargaitem`, `subtotal`, `pengiriman`, `metode`, `status`) VALUES
-('TRS0000001', 'PDK0000005', 'altix', 15, 15000, 15000, 'Antar ke Kiev, Ukraina', 'Tunai', '0');
+INSERT INTO `detail_transaksi` (`id_transaksi`, `id_produk`, `jumlah`, `totalhargaitem`) VALUES
+('TRS0000001', 'PDK0000005', 15, 15000),
+('TRS0000001', 'PDK0000001', 2, 10000);
 
 -- --------------------------------------------------------
 
@@ -73,8 +69,8 @@ INSERT INTO `produk` (`id_produk`, `nama_produk`, `deskripsi_produk`, `harga`, `
 ('PDK0000004', 'Tempe Bacem', 'Tempe goreng dengan bumbu gula merah yang manis dan gurih', 1500, 450, 'cemilan', 'tempe1.jpeg'),
 ('PDK0000005', 'Sate Usus', 'Sate yang terbuat dari usus ayam', 1000, 600, 'cemilan', 'sate-usus.jpg'),
 ('PDK0000006', 'Teh Hangat', 'Segelas Teh Hangat yang manis', 2500, 740, 'minumanpanas', 'teh-hangat.jpg'),
-('PDK0000007', 'Jeruk Hangat', 'Segelas Jeruk manis yang dapat menghangatkan tubuh', 3000, 590, 'minumanpanas', 'jeruk-hangat.jpg'),
-('PDK0000008', 'Es Teh', 'Teh Dingin dengan rasa manis menyegarkan', 3000, 770, 'minumandingin', 'es-teh.jpeg');
+('PDK0000007', 'Jeruk Hangat', 'Segelas Jeruk manis yang dapat menghangatkan tubuh', 3000, 500, 'minumanpanas', 'jeruk-hangat.jpg'),
+('PDK0000008', 'Es Teh', 'Teh Dingin dengan rasa manis menyegarkan', 3000, 700, 'minumandingin', 'es-teh.jpeg');
 
 -- --------------------------------------------------------
 
@@ -84,6 +80,11 @@ INSERT INTO `produk` (`id_produk`, `nama_produk`, `deskripsi_produk`, `harga`, `
 
 CREATE TABLE `transaksi` (
   `id_transaksi` char(10) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `subtotal` bigint(15) DEFAULT NULL,
+  `pengiriman` varchar(150) DEFAULT NULL,
+  `metode` enum('Tunai','COD','Transfer') DEFAULT NULL,
+  `status` enum('0','1','2') DEFAULT NULL,
   `tanggal_transaksi` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -91,8 +92,8 @@ CREATE TABLE `transaksi` (
 -- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi` (`id_transaksi`, `tanggal_transaksi`) VALUES
-('TRS0000001', '2022-12-07');
+INSERT INTO `transaksi` (`id_transaksi`, `username`, `subtotal`, `pengiriman`, `metode`, `status`, `tanggal_transaksi`) VALUES
+('TRS0000001', 'altix', 15000, 'Nganjuk', 'Tunai', '0', '2022-12-08');
 
 -- --------------------------------------------------------
 
@@ -107,7 +108,7 @@ CREATE TABLE `user` (
   `nama_lengkap` varchar(30) NOT NULL,
   `no_hp` bigint(13) NOT NULL,
   `alamat` varchar(100) NOT NULL,
-  `level` enum('admin','pelanggan') DEFAULT NULL
+  `level` enum('admin','pelanggan','''') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -115,9 +116,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `email`, `password`, `nama_lengkap`, `no_hp`, `alamat`, `level`) VALUES
-('altix', 'variousra@gmail.com', '123', 'Maulana Akbar', 85236167349, 'Kawedanan, Magetan', 'admin'),
-('ansori', 'aanblogme@gmail.com', 'ansori123', 'A. Ansori', 81234567890, 'Tanjunganom, Nganjuk', 'admin'),
-('firdaus', 'firdaus@gmail.com', '123', 'Ahmad Firdaus T', 85748372312, 'Banyuwangi', 'pelanggan');
+('altix', 'variousra@gmail.com', '123', 'Maulana Akbar', 85236167349, 'Kawedanan, Magetan', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -128,8 +127,7 @@ INSERT INTO `user` (`username`, `email`, `password`, `nama_lengkap`, `no_hp`, `a
 --
 ALTER TABLE `detail_transaksi`
   ADD KEY `id_transaksi` (`id_transaksi`),
-  ADD KEY `id_produk` (`id_produk`),
-  ADD KEY `username` (`username`);
+  ADD KEY `id_produk` (`id_produk`);
 
 --
 -- Indexes for table `produk`
@@ -141,7 +139,8 @@ ALTER TABLE `produk`
 -- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD PRIMARY KEY (`id_transaksi`);
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `username` (`username`);
 
 --
 -- Indexes for table `user`
@@ -158,8 +157,13 @@ ALTER TABLE `user`
 --
 ALTER TABLE `detail_transaksi`
   ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`),
-  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`),
-  ADD CONSTRAINT `detail_transaksi_ibfk_3` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
